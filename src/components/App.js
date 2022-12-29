666155c04af2ea52fc815dc4d3c7e58185d9bdb6
Akhil chandra <akhilchandra2309@gmail.com>
2022-12-29 15:39:37 +0530

    solution

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,8 @@ import '../styles/App.css';
 const App = () => {
   return (
     <div id="main">
-    <div id="root">"I am learning React. My life is getting better. "</div>
+    <div id="root">
+    <p>"I am learning React. My life is getting better. "</p></div>
     </div>
   )
 }
